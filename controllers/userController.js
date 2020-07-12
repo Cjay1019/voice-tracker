69d74e8a8804174a321c2f6a18b85f8cc9f98c0a
Cjay1019 <cjay741@gmail.com>
2020-07-12 17:51:37 -0400

    started auth work with passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,8 @@ const User = require("../models/user");
 
 module.exports = {
     create: function (req, res) {
-        console.log(User)
         User.create(req.body).then(dbResult => {
-            res.json(dbResult);
+            res.json({ message: "Signup successful", user: req.user });
         });
     }
 }
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const userController = require("../controllers/userController");
 
 module.exports = function (app) {
     // express routes
-    app.post("/api/create", userController.create);
+    app.post("/api/signup",
+        passport.authenticate("signup", { session: false }),
+        userController.create);
 
     // app.get("/api/:id", userController.find);
 
