0973caa15ea5db4a845138b10517546fdb8d8141
Cjay1019 <cjay741@gmail.com>
2020-07-29 22:28:47 -0400

    started work on audio recording in create form

--- client/src/components/CharacterForm.js
+++ client/src/components/CharacterForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@material-ui/core/';
+import { TextField, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, recomposeColor } from '@material-ui/core/';
 
 
 function CharacterForm({ formOpen, setFormOpen }) {
@@ -8,28 +8,38 @@ function CharacterForm({ formOpen, setFormOpen }) {
 
     return (
         <Dialog open={formOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <DialogTitle id="form-dialog-title">Create new character</DialogTitle>
             <DialogContent>
-                <DialogContentText>
-                    To subscribe to this website, please enter your email address here. We will send updates
-                    occasionally.
-          </DialogContentText>
                 <TextField
+                    autoComplete="off"
                     autoFocus
                     margin="dense"
                     variant="outlined"
                     id="name"
-                    label="Email Address"
-                    type="email"
+                    label="Name"
+                    type="text"
                     fullWidth
                 />
+                <TextField
+                    autoComplete="off"
+                    multiline
+                    rows={4}
+                    rowsMax={8}
+                    margin="dense"
+                    variant="outlined"
+                    id="description"
+                    label="Description"
+                    type="text"
+                    fullWidth
+                />
+                <Button onClick={() => setRecording(!recording)}>Record</Button>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="primary">
                     Cancel
           </Button>
                 <Button onClick={handleClose} color="primary">
-                    Subscribe
+                    Create
           </Button>
             </DialogActions>
         </Dialog>
