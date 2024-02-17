190ccbba4aedfae69f51b90ec220c0062e0c9a45
Conner Leigh <cjay741@gmail.com>
2024-02-17 16:03:02 -0500

    changing of paths

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(cookieParser());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/build")));
-    app.use(express.static(path.join(__dirname, "/files")));
     require('dotenv').config();
     console.log("env success");
 };
